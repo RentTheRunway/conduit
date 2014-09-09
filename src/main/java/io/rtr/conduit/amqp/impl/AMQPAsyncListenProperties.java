8f98c5053937f2a0da2b519982ae9d57520e67ba
Carlo <carlo@renttherunway.com>
2014-09-09 11:25:41 -0400

    INFRA-1361 there was 1 more public constructor

--- src/main/java/io/rtr/conduit/amqp/impl/AMQPAsyncListenProperties.java
+++ src/main/java/io/rtr/conduit/amqp/impl/AMQPAsyncListenProperties.java
@@ -5,7 +5,7 @@ import io.rtr.conduit.amqp.AMQPAsyncConsumerCallback;
 public class AMQPAsyncListenProperties extends AMQPCommonListenProperties {
     private AMQPAsyncConsumerCallback callback;
 
-    public AMQPAsyncListenProperties(AMQPAsyncConsumerCallback callback, String exchange, String queue, int threshold, int prefetchCount, boolean poisonQueueEnabled, boolean purgeOnConnect, boolean dynamicQueueCreation, String poisonPrefix, String dynamicQueueRoutingKey) {
+    AMQPAsyncListenProperties(AMQPAsyncConsumerCallback callback, String exchange, String queue, int threshold, int prefetchCount, boolean poisonQueueEnabled, boolean purgeOnConnect, boolean dynamicQueueCreation, String poisonPrefix, String dynamicQueueRoutingKey) {
         super(exchange, queue, threshold, prefetchCount, poisonQueueEnabled, purgeOnConnect, dynamicQueueCreation, poisonPrefix, dynamicQueueRoutingKey);
         this.callback = callback;
     }
