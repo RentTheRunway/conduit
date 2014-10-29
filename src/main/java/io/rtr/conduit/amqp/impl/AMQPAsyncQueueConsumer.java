13385286c76184b6f0a54d016f23d89938dc818b
Zain Cheng <zcheng@renttherunway.com>
2014-10-29 17:20:48 -0400

    CXP-654: Conduit/Notification Service: Support reason field

--- src/main/java/io/rtr/conduit/amqp/impl/AMQPAsyncQueueConsumer.java
+++ src/main/java/io/rtr/conduit/amqp/impl/AMQPAsyncQueueConsumer.java
@@ -6,7 +6,7 @@ import com.rabbitmq.client.Envelope;
 import com.rabbitmq.client.ShutdownSignalException;
 import io.rtr.conduit.amqp.AMQPAsyncConsumerCallback;
 import io.rtr.conduit.amqp.AMQPMessageBundle;
-import io.rtr.conduit.amqp.Action;
+import io.rtr.conduit.amqp.ActionResponse;
 import io.rtr.conduit.amqp.AsyncResponse;
 import org.apache.log4j.Logger;
 
@@ -53,7 +53,7 @@ public class AMQPAsyncQueueConsumer extends AMQPQueueConsumer implements AsyncRe
         }
     }
 
-    protected void respond(AMQPMessageBundle messageBundle, Action action, boolean multiple) {
+    protected void respond(AMQPMessageBundle messageBundle, ActionResponse actionResponse, boolean multiple) {
         //! We can't issue any blocking amqp calls in the context of this method, otherwise
         //  channel's internal thread(s) will deadlock. Both, basicAck and basicReject are
         //  asynchronous.
@@ -61,9 +61,12 @@ public class AMQPAsyncQueueConsumer extends AMQPQueueConsumer implements AsyncRe
         Long deliveryTag = envelope.getDeliveryTag();
         byte[] body = messageBundle.getBody();
         AMQP.BasicProperties properties = messageBundle.getBasicProperties();
+        if(actionResponse.getReason()!=null && !actionResponse.getReason().trim().isEmpty()) {
+            properties = createCopyWithActionReason(properties, actionResponse.getReason());
+        }
 
         try {
-            switch (action) {
+            switch (actionResponse.getAction()) {
                 case Acknowledge:
                     ack(deliveryTag, multiple);
                     break;
@@ -190,12 +193,12 @@ public class AMQPAsyncQueueConsumer extends AMQPQueueConsumer implements AsyncRe
     }
 
     @Override
-    public void respondMultiple(AMQPMessageBundle messageBundle, Action action) {
-        respond(messageBundle, action, true);
+    public void respondMultiple(AMQPMessageBundle messageBundle, ActionResponse actionResponse) {
+        respond(messageBundle, actionResponse, true);
     }
 
     @Override
-    public void respondSingle(AMQPMessageBundle messageBundle, Action action) {
-        respond(messageBundle, action, false);
+    public void respondSingle(AMQPMessageBundle messageBundle, ActionResponse actionResponse) {
+        respond(messageBundle, actionResponse, false);
     }
 }
